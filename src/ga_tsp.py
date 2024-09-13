e8bf2ea2e9b03536a9b2254d5a5ede672e33db1e
longhao qian <longhao.qian@maill.utoronto.ca>
2024-09-13 08:52:21 -0400

    add tsp algorithm

--- src/ga_tsp.py
+++ src/ga_tsp.py
@@ -6,16 +6,37 @@ def CalcDistance(p1, p2):
 
 
 if __name__ == '__main__':
-    # define the starting location
-    start = np.array([0, 0])
-    num = 10 # number of sites
-    dist = np.zeros((num + 1, num + 1))
-    location = np.zeros((num, 2))
-    # load the locations
-    location[0, :] = np.array([10, 12])
-    
 
-    print(location)
+    popSize = 2500 # population size
+    genNum = 1000 # number of generation size
+    mutSiteP = 0.01 # probability of exchange 2 random sites in the path (per gene, per genration)
+    mutpathP = 0.02 # probability of exchange 2 peices of path (per gene, per genration)
+    flipPathP = 0.08 # probability of flip random pieces of path
+    
+    startLocation = np.array([0, 0])
+    location = np.array([[12, 4],
+                         [33, 0],
+                         [5, 4],
+                         [9, 16],
+                         [8, 0],
+                         [10, 15],
+                         [7, 9],
+                         [4, 21],
+                         [14, 10],
+                         [20, 18]])/10
+    
+    numSites = location.shape[0]
+    # distance between each site
+    distM = np.zeros((numSites, numSites))
+    # distance from the starting point to each site
+    distS = np.zeros((numSites, 1))
+    for i in range(numSites):
+        distS[i] = np.linalg.norm(startLocation - location[i])
+        for j in range(numSites):
+            distM[i][j] = np.linalg.norm(location[i] - location[j])
+
+    print(distM)
+
 
     # plt.figure(figsize=(5,5), dpi= 100)
     
