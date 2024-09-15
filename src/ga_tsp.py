acbb5694bd9913cc7e5af9eafe59e6dff8a88db8
longhaao qian <longhao.qian@mail.utoronto.ca>
2024-09-15 19:18:30 -0400

    add genetic algorithm

--- src/ga_tsp.py
+++ src/ga_tsp.py
@@ -1,10 +1,62 @@
 import numpy as np
 import ga_lib as gl
 import matplotlib.pyplot as plt
+import random
+
+def perform_cross_over(popSize, popProb, num_of_sites, genes):
+    cross_over_indices = gl.get_roulette_wheel_indexes(popSize, popProb)
+    child_genes = np.zeros((popSize, numSites))
+    for i in range(popSize / 2):
+        # perform cross_over for each pair of parent genes
+        i1 = 2 * i
+        i2 = 2 * i + 1
+        # get the index of genes
+        i1_g = cross_over_indices[i1]
+        i2_g = cross_over_indices[i2]
+        # generate a crossover point
+        cp = random.randint(0,  num_of_sites - 2)
+        # generate 2 children
+        child_genes[i1] = gl.insert_at_beginning(genes[i1_g], genes[i2_g], cp)
+        child_genes[i2] = gl.insert_at_beginning(genes[i2_g], genes[i1_g], cp)
+    return child_genes
+
+
+def perform_mutation(popSize, num_of_sites, genes, mute_sites, mute_path, mute_flip):
+    # 1. randomly exchange 2 sites
+    for i in range(popSize):
+        if random.uniform(0.0, 1.0) < mute_sites:
+            # random number of sites
+            rns = random.randint(0, num_of_sites - 1)
+            # use the first chunk of the random sequence
+            rnss = np.random.permutation(num_of_sites)
+            ctp = rnss[0 : rns]
+            g_list = genes[i].tolist()
+            gt = [g_list[j] for j in ctp]
+            # hash the sites
+            sp = np.random.permutation(rns)
+            gt2 = [gt[j] for j in sp]
+            for j in range(ctp):
+                genes[i][j] = gt2[j]
+
+    # 2. randomly exchange 2 segments of path
+    for i in range(popSize):
+        if random.uniform(0.0, 1.0) < mute_path:
+            cp = random.randint(1, num_of_sites - 2)
+            g1 = genes[i][0: cp]
+            g2 = genes[i][cp + 1:]
+            genes[i] = np.concatenate(g2, g1)
+
+    # 3. randomly flip a piece of path
+    for i in range(popSize):
+        if random.uniform(0.0, 1.0) < mute_flip:
+            n1 = random.randint(0, num_of_sites - 1)
+            n2 = random.randint(n1, num_of_sites - 1)
+            genes[i][n1 : n2 + 1] = np.flip(genes[i][n1 : n2 + 1])
+
 
 if __name__ == '__main__':
 
-    popSize = 2500 # population size
+    popSize = 2500 # population size (should be an even number)
     genNum = 1000 # number of generation size
     mutSiteP = 0.01 # probability of exchange 2 random sites in the path (per gene, per genration)
     mutpathP = 0.02 # probability of exchange 2 peices of path (per gene, per genration)
@@ -44,6 +96,17 @@ if __name__ == '__main__':
     pathLength = np.zeros((popSize, 1))
     # gene probability to be selected
     probP = np.zeros((popSize, 1))
+
+    # initiation plot
+    plt.ion()  # turning interactive mode on
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+    for i in range(numSites):
+        ax.scatter(location[i][0], location[i][1], c='g')
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
     for ig in range(genNum):
         # step 1, find total path distance including the start location
         for ip in range(popSize):
@@ -61,9 +124,15 @@ if __name__ == '__main__':
         cross_over_indices = gl.get_roulette_wheel_indexes(popSize, probP)
         # reset children
         child_genes.fill(0)
+        # choose parents to crossover
+        # set children to population
+        genes = perform_cross_over(popSize, probP, numSites, genes)
+        # perform mutation
+        perform_mutation(popSize, numSites, genes, mutSiteP, mutpathP, flipPathP )
+        # elitism
+        genes[0] = best_gene
 
 
-
-    # print(genes)
+        # print(genes)
     # plt.figure(figsize=(5,5), dpi= 100)
     
